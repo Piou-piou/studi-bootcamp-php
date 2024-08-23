495ed7379c0ceb07b1b7e929d90879ecb27b67e1
Piou-piou <pilloud.anthony@gmail.com>
2024-08-23 12:49:01 +0200

    feat: teams ajout router

---
+++ day5/teams/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="index.php?page=home">lien vers index</a>
+                <a href="index.php?page=ajout_voiture">Ajouter une voiture</a>
+            </nav>
+        </header>
+
+        <main>
+            <?php require_once 'pages/'.$_GET['page'].'.php'?>
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
