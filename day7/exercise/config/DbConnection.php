5c054c47bf6ec66a0e1baa4ad80fc4f63ee80e90
Anthony Pilloud <anthony.pilloud@quarksgroup.com>
2024-08-27 09:49:17 +0200

    preparation dbconnection

--- day7/exercise/config/DbConnection.php
+++ day7/exercise/config/DbConnection.php
@@ -2,5 +2,20 @@
 
 class DbConnection
 {
+    const DSN = 'mysql:host=mysql;dbname=garage';
+    const USER = 'root';
+    const PASSWORD = '';
 
+    static ?PDO $pdo = null;
+
+    public static function getPdo(): PDO
+    {
+        if (self::$pdo !== null) {
+            return self::$pdo;
+        }
+
+        self::$pdo = new PDO(self::DSN, self::USER, self::PASSWORD);
+
+        return self::$pdo;
+    }
 }
