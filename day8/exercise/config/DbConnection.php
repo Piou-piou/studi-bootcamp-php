9d522932f657a6ac0e6e00c28ed46538757952ff
Anthony Pilloud <anthony.pilloud@quarksgroup.com>
2024-08-28 09:16:00 +0200

    feat: prepare day8

---
+++ day8/exercise/config/DbConnection.php
@@ -0,0 +1,26 @@
+<?php
+
+class DbConnection
+{
+    const DSN = 'mysql:host=127.0.0.1;dbname=garage';
+    const USER = 'root';
+    const PASSWORD = '';
+
+    // définition de notre variable qui stockera notre PDO
+    static ?PDO $pdo = null;
+
+    public static function getPdo(): PDO
+    {
+        // si pdo existe et a déjà été rempli avec le new PDO on le retourne directement pour éviter
+        // d'enregistrer x pdo et ouvrir x connexion à notre bdd
+        if (self::$pdo !== null) {
+            return self::$pdo;
+        }
+
+        // ici seulement si self::pdo n'est pas défini on ouvre notre connexion à la base
+        self::$pdo = new PDO(self::DSN, self::USER, self::PASSWORD);
+
+        // et on retourne la valeur
+        return self::$pdo;
+    }
+}
