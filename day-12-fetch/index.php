0f8124c64998d475d5f68983d1a4baa06b940661
Anthony Pilloud <anthony.pilloud@quarksgroup.com>
2024-09-05 10:27:50 +0200

    feat: feat: add Router class

--- day-12-fetch/index.php
+++ day-12-fetch/index.php
@@ -1,67 +1,9 @@
 <?php
 
+use App\Routing\Router;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-// via le .htacess on redirige toujours toutes les pages sur ce fichier
-// index.php pour gérer l'affichage comme on le souhaite
-// et aller chercher le controller et la méthode de manière automatique
+$router = new Router($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 
-// ic on récupère l'URI de l'url
-// uri = partie interne de l"url, ce qiu se situe après https://monsite.com
-$uri = $_SERVER['REQUEST_URI'];
-
-// explode transforme une chaine de caractère en tableau en fondction d'un séprateur
-// ici le /
-// ici je choisi de séparer mon URi en 2 parties
-// premier partie = MonControler
-// seconde partie la méthode du controller
-$uriParts = explode('/', $uri);
-
-
-// si pas d'uriParts alors onn est sur l'accueil
-if (count($uriParts) === 2 && $uriParts[1] === '') {
-    $uriParts[1] = 'homepage';
-    $uriParts[2] = 'home';
-}
-
-// on enelve la premiere partie car l'uri commence par un slash donc vide
-unset($uriParts[0]);
-
-// ici on ajoute un majusccule au premier caractère car une class php doit toujours commencer par une maj
-$controller = ucfirst($uriParts[1]);
-
-// la partie 2 correspond à la méthode appelée
-$method = $uriParts[2];
-
-// la partie permet de récupérer un paramètre qui sera donné à la méthode du controller
-$parameter = $uriParts[3] ?? null;
-
-// App\Controller\Inscrption pour la page inscription
-// App\Controller\Login pour la page login
-// ucfisrt permet de mettre la première lettre en majuscule
-$controllerName = 'App\\Controller\\'.$controller;
-
-// ici on instancie la class qui va gérer notre formulaire et ses erreurs
-$controller = new $controllerName();
-
-$result = null;
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // pour gérer les post de formulaire
-    if (null !== $parameter) {
-        $result = $controller->$method($parameter, $_POST);
-    } else {
-        $result = $controller->$method($_POST);
-    }
-} else {
-    if (null !== $parameter) {
-        $result = $controller->$method($parameter);
-    } else {
-        $result = $controller->$method();
-    }
-}
-
-
-
-echo json_encode($result);
+echo $router->doAction();
--- day-12-fetch/src/Controller/Api/Voiture.php
+++ day-12-fetch/src/Controller/Api/Voiture.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Database\Dbutils;
 use PDO;
